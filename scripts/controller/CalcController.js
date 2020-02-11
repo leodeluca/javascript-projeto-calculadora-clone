d0614c2abbee64171186dbcd615cffa846b0fb17
leodeluca <leocluca@gmail.com>
2020-02-11 19:07:28 -0300

    class 11 commit

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,76 @@
+class CalcController{
+
+    constructor(){
+
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+
+    }
+
+    initialize(){
+
+        this.setDisplayDateTime();
+
+        setInterval(() => {
+
+            this.setDisplayDateTime();
+
+        }, 1000);
+
+    }
+
+    initButtonsEvents(){
+
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+
+    }
+
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    set currentDate(value){
+        this.currentDate = value;
+    }
+
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+
+    set displayDate(value){
+        this._dateEl.innerHTML = value;
+    }
+
+}
